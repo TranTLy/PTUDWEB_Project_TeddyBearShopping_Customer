c6f1ecb3648620f83fc1dc30964ab4eb6b81aa09
TranLy <1612367@student.hcmus.edu.vn>
2019-05-13 23:10:02 +0700

    fix connect to server

--- dbs/index.js
+++ dbs/index.js
@@ -1,32 +1,3 @@
-// // const MongoClient = require("mongodb").MongoClient;
-// // const URI = "mongodb+srv://admin:admin123@cluster0-yxmrz.mongodb.net/ToyShopDb";
-
-// // var dbs = { production: {} };
-
-// // function connect(url) {
-// //   return MongoClient.connect(url).then(client => client.db());
-// // }
-
-// // exports.initdb = async function() {
-// //   // let databases = await Promise.all([connect(PROD_URI), connect(MKTG_URI)])
-// //   let database = await connect(URI);
-// //   dbs.production = database;
-// // };
-
-// // exports.dbs = dbs;
-// const MongoClient = require("mongodb").MongoClient;
-// const uri = "mongodb+srv://admin:admin123@cluster0-yxmrz.mongodb.net/test";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// let db = {};
-// client.connect(err => {
-//   db = client.db("ToyShopDB");
-//   //   console.log("test db", db.collection("products"));
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-// exports.db = db;
-
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
