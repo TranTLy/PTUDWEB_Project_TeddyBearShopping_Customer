097de703e7c115eddbfecad2aaaae768990042be
TranLy <1612367@student.hcmus.edu.vn>
2019-05-27 18:53:43 +0700

    Dang ky/ dang nhap thanh cong

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,6 @@
 const User = require('../model/user');
 var passport = require('passport');
-var bcrypt = require('bcrypt-nodejs');
+var Bcrypt = require('bcryptjs');
 var config = require('../config/database');
 require('../config/passport')(passport);
 var jwt = require('jsonwebtoken');
@@ -17,87 +17,60 @@ exports.change_password = function(req, res) {
 exports.update_infor = function(req, res) {
 	res.render('customer-views/update-infor', { title: 'Thay đổi thông tin' });
 };
-function generateHashPassword(password) {
-	bcrypt.genSalt(10, (err, salt) => {
-		bcrypt.hash(password, salt, null, (err, hash) => {
-			console.log('password from hash: ', hash);
-			return hash;
-		});
-	});
-}
-exports.post_signin = function(req, res) {
-	const email = req.body.email;
-	const password = req.body.password;
-	//console.log('email: ', email, 'pass: ', password);
-	User.findOne({ email }, (err, user) => {
-		if (err) throw err;
-		if (!user) {
-			console.log('Không tồn tại user');
-			res.status(401).send({ success: false, msg: 'Tên đăng nhập hoặc mật khẩu sai.' });
-		} else {
-			// check if password matches
-			console.log('pass in db: ', user.password);
-			if (generateHashPassword(password) == user.password) {
+
+exports.post_signin = async function(req, res) {
+	try {
+		await User.findOne({ email: req.body.email }, (err, user) => {
+			if (err) throw err;
+			if (!user) {
+				//user isn't exist
+				res.status(401).send({ success: false, msg: 'Tên đăng nhập hoặc mật khẩu sai.' });
+			} else {
+				if (!Bcrypt.compareSync(req.body.password, user.password)) {
+					//wrong password
+					return res.status(400).send({ message: 'Tên đăng nhập hoặc mật khẩu không đúng' });
+				}
 				var token = jwt.sign(user.toJSON(), config.secret, {
-					expiresIn: 604800 // 1 week
+					expiresIn: 86400 // 1 day
 				});
 				// return the information including token as JSON
-				res.json({ success: true, token: 'JWT ' + token });
-			} else {
-				//Wrong passport
-				console.log('Sai pass');
-				res.status(401).send({ success: false, msg: 'Tên đăng nhập hoặc mật khẩu sai.' });
+				res.json({ success: true, token: 'JWT ' + token, message: 'Đăng nhập thành công' });
 			}
-			// user.comparePassword(password, function(err, isMatch) {
-			// 	if (isMatch && !err) {
-			// 		// if user is found and password is right create a token
-			// 		var token = jwt.sign(user.toJSON(), config.secret, {
-			// 			expiresIn: 604800 // 1 week
-			// 		});
-			// 		// return the information including token as JSON
-			// 		res.json({ success: true, token: 'JWT ' + token });
-			// 		// res.render('customer-views/index', { title: 'Trang chủ' });
-			// 	} else {
-			// 		//Wrong passport
-			// 		console.log('Sai pass');
-			// 		res.status(401).send({ success: false, msg: 'Tên đăng nhập hoặc mật khẩu sai.' });
-			// 	}
-			// });
-		}
-	});
+		});
+	} catch (err) {
+		res.status(500).send(err);
+	}
 };
 
-exports.post_signup = function(req, res) {
+exports.post_signup = async function(req, res) {
 	if (!req.body.name || !req.body.password || req.body.repassword != req.body.password) {
-		console.log('user empty');
-		//res.render('customer-views/signup', { title: 'Đăng ký' });
-		res.status(401).send({ success: false, msg: 'Điền thông tin chưa chính xác.' });
+		res.render('customer-views/signup', {
+			title: 'Đăng ký thất bại',
+			message: 'Điền thông tin chưa chính xác'
+		});
 	} else {
-		const email = req.body.email;
-		const userExist = User.findOne({ email: email }, (err, results) => {
-			console.log('user exist ', results);
+		User.findOne({ email: req.body.email }, (err, results) => {
 			if (results != null) {
 				res.render('customer-views/signup', {
 					title: 'Đăng ký thất bại',
-					message: `Email ${email} đã tồn tại. Vui lòng dùng email này để đăng nhập hoặc đăng ký tài khoản bằng email khác.`
+					message: `Email ${req.body
+						.email} đã tồn tại. Vui lòng dùng email này để đăng nhập hoặc đăng ký tài khoản bằng email khác.`
 				});
 			} else {
-				const newUser = {
-					name: req.body.name,
-					password: generateHashPassword(req.body.password),
-					email: req.body.email
-				};
-				const user = new User(newUser);
-				user.save().then((objext) => {
-					const x = User.findOne({ email: req.body.email }, (err, x) => {
-						console.log('new password: ', x.password, '- name: ', x.name, '- email: ', x.email);
-					});
-					res.render('customer-views/update-infor', {
-						title: 'Thay đổi thông tin',
-						message: 'Đăng ký thành công',
-						name: req.body.name
-					});
+				req.body.password = Bcrypt.hashSync(req.body.password, 10);
+				const user = new User({ name: req.body.name, password: req.body.password, email: req.body.email });
+
+				var result = user.save();
+				res.render('customer-views/update-infor', {
+					title: 'Thay đổi thông tin',
+					message: 'Đăng ký thành công',
+					name: req.body.name
 				});
+
+				// //  create token
+				// var token = jwt.sign(user.toJSON(), config.secret, {
+				// 	expiresIn: 86400 // 1 day
+				// });
 			}
 		});
 	}
--- model/user.js
+++ model/user.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
 var SchemaTypes = mongoose.Schema.Types;
 
 var UserSchema = new Schema(
@@ -26,39 +25,5 @@ var UserSchema = new Schema(
 	},
 	{ collection: 'customers' }
 );
-
-// UserSchema.pre('save', (next) => {
-// 	var user = this;
-// 	//	if (this.isModified('password') || this.isNew) {
-// 	bcrypt.genSalt(10, (err, salt) => {
-// 		if (err) {
-// 			return next(err);
-// 		}
-// 		bcrypt.hash(user.password, salt, null, (err, hash) => {
-// 			if (err) {
-// 				return next(err);
-// 			} else {
-// 				user.password = hash;
-// 				console.log('password hash: ', user.password);
-// 				return next();
-// 			}
-// 		});
-// 	});
-// 	// }
-// 	// else {
-// 	// 	return next();
-// 	// }
-// });
-
-// UserSchema.methods.comparePassword = (pass, cb) => {
-// 	//console.log('this password: ', this.password);
-// 	bcrypt.compare(pass, this.password, (err, isMatch) => {
-// 		if (err) {
-// 			return cb(err);
-// 		} else {
-// 			return cb(null, isMatch);
-// 		}
-// 	});
-// };
 
 module.exports = mongoose.model('User', UserSchema);
